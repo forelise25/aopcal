46f9fc91770571ae0eae781122e5e2812f83264e
Garam Son <forelise25@naver.com>
2018-06-22 08:58:37 +0900

    XML 스키마 기반으로 AOP 구현

--- src/main/java/kr/hs/emirim/forelise25/aopcal/main/MainProxy.java
+++ src/main/java/kr/hs/emirim/forelise25/aopcal/main/MainProxy.java
@@ -4,11 +4,12 @@ import org.springframework.util.ExceptionTypeFilter;
 
 import kr.hs.emirim.forelise25.aopcal.cal.ExeTimeCalculator;
 import kr.hs.emirim.forelise25.aopcal.cal.ImpCalculator;
+import kr.hs.emirim.forelise25.aopcal.cal.RecCalculator;
 
 public class MainProxy {
 
 	public static void main(String[] args) {
-		ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpeCalculator());
+		ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpCalculator());
 		System.out.println(ttCal1.factorial(20));
 
 		ExeTimeCalculator ttCal2 = new ExeTimeCalculator(new RecCalculator());
